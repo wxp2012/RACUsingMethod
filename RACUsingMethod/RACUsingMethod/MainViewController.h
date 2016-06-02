0293373c687ed1ead96264ed089b80b2a0696249
wxp2012 <1689755992@qq.com>
2016-06-02 10:43:31 +0800

    添加工程

---
+++ RACUsingMethod/RACUsingMethod/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACUsingMethod
+//
+//  Created by aipai on 16/6/2.
+//  Copyright © 2016年 xp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
---
+++ RACUsingMethod/RACUsingMethod/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RACUsingMethod
+//
+//  Created by aipai on 16/6/2.
+//  Copyright © 2016年 xp. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
