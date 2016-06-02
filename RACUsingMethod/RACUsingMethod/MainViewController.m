3e961286c7bec37bae8787ad76d3e8231a011f63
wxp2012 <1689755992@qq.com>
2016-06-02 11:46:34 +0800

    添加2~4项

--- RACUsingMethod/RACUsingMethod/MainViewController.m
+++ RACUsingMethod/RACUsingMethod/MainViewController.m
@@ -12,6 +12,7 @@
 @interface MainViewController ()
 
 @property (nonatomic,strong) NSString *inputStr;
+@property (nonatomic,strong) NSString *isConnected;
 
 @end
 
@@ -21,7 +22,7 @@
     [super viewDidLoad];
     
     //1、监听对象的成员变量变化，当成员变量值被改变时，触发做一些事情。
-    //场景：当前类有一个成员变量 NSString *input，当它的值被改变时，发送一个请求。
+    //场景：当前类有一个成员变量 NSString *input，当它的值被改变时打印。
     [RACObserve(self, self.inputStr)
          subscribeNext:^(id x) {
              if (x) {
@@ -32,6 +33,85 @@
      ];
     self.inputStr = @"changed";
     
+    //2、场景：在上面场景中，当用户输入的值以2开头时，才打印.
+    self.inputStr = @"2changed";
+    [[RACObserve(self, self.inputStr)
+        filter:^BOOL(id value) {
+            if ([value hasPrefix:@"2"]) {
+                return YES;
+            }
+            else
+            {
+                return NO;
+            }
+        }
+     ]
+    subscribeNext:^(id x) {
+        if (x) {
+            NSLog(@"有2开头的字符串");
+        }
+    }];
+    
+    //3、场景：上面场景是监听自己的成员变量，如果想监听UITextField输入值变化，框架也做了封装可以代替系统回调
+    UITextField *textF = [[UITextField alloc] initWithFrame:CGRectMake(20, 30, 150, 30)];
+    textF.backgroundColor = [UIColor colorWithRed:241/255.f green:241/255.f blue:241/255.f alpha:1.0];
+    [self.view addSubview:textF];
+    
+    [[[textF rac_textSignal]
+         filter:^BOOL(id value) {
+             
+             NSString *str = value;
+             if (str.integerValue > 20) {
+                 return YES;
+             }
+             else
+             {
+                 return NO;
+             }
+         }]
+    subscribeNext:^(id x) {
+        if (x) {
+            NSLog(@"输入的值大于20了");
+        }
+    }];
+    
+    
+    //4、同时监听多个变量变化，当这些变量满足一定条件时，使button为可点击状态
+    UITextField *textF2 = [[UITextField alloc] initWithFrame:CGRectMake(20, 70, 150, 30)];
+    textF2.backgroundColor = [UIColor colorWithRed:241/255.f green:241/255.f blue:241/255.f alpha:1.0];
+    [self.view addSubview:textF2];
+    
+    UIButton *clickedBtn = [[UIButton alloc] initWithFrame:CGRectMake(30, 115, 130, 30)];
+    [clickedBtn setTitle:@"点击" forState:UIControlStateNormal];
+    [clickedBtn setBackgroundImage:[self imageWithColor:[UIColor grayColor]] forState:UIControlStateDisabled];
+    [clickedBtn setBackgroundImage:[self imageWithColor:[UIColor colorWithRed:254/255.f green:115/255.f blue:85/255.f alpha:1.0]] forState:UIControlStateNormal];
+    [self.view addSubview:clickedBtn];
+    
+    self.isConnected = @"1";
+    RAC(clickedBtn,enabled) = [RACSignal
+                                   combineLatest:@[textF.rac_textSignal,
+                                                   textF2.rac_textSignal,
+                                                   RACObserve(self, self.isConnected)
+                                                   ]
+                                   reduce:^(NSString *price, NSString *name, NSNumber *connect){
+                                       return @(price.length > 0 && name.length > 0 && [connect boolValue]);
+                                   }];
+    
+}
+
+//颜色转换 背景图片
+- (UIImage *)imageWithColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
     
 }
 
